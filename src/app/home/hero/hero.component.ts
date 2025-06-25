c4216534f85d27b870afb6ef871bc9dbceb20771
Marco Scazzari <marco.scazzari@lefonti.com>
2025-06-25 17:59:26 +0200

    Effetto typing text automatico + componente services

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { GoTopComponent } from "../../shared/go-top/go-top.component";
+import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
+
+import { GoTopComponent } from '../../shared/go-top/go-top.component';
 import { HERO_LINKS } from './hero-links';
 
 @Component({
@@ -8,8 +9,45 @@ import { HERO_LINKS } from './hero-links';
   standalone: true,
   imports: [GoTopComponent, NgFor],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
-export class HeroComponent {
-  links= HERO_LINKS;
+export class HeroComponent implements OnInit {
+  links = HERO_LINKS;
+
+  texts: string[] = ['web developer', 'design', 'codice', 'SEO', 'frontend'];
+  currentText = '';
+  currentWordIndex = 0;
+  currentCharIndex = 0;
+  isDeleting = false;
+
+  ngOnInit(): void {
+    this.typeEffect();
+  }
+
+  typeEffect(): void {
+    const fullText = this.texts[this.currentWordIndex];
+
+    if (this.isDeleting) {
+      this.currentCharIndex--;
+      this.currentText = fullText.substring(0, this.currentCharIndex);
+    } else {
+      this.currentCharIndex++;
+      this.currentText = fullText.substring(0, this.currentCharIndex);
+    }
+
+    let typingSpeed = this.isDeleting ? 75 : 100;
+
+    if (!this.isDeleting && this.currentCharIndex === fullText.length) {
+      // Pausa alla fine della parola
+      typingSpeed = 1500;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.currentCharIndex === 0) {
+      // Passa alla parola successiva
+      this.isDeleting = false;
+      this.currentWordIndex = (this.currentWordIndex + 1) % this.texts.length;
+      typingSpeed = 500;
+    }
+
+    setTimeout(() => this.typeEffect(), typingSpeed);
+  }
 }
