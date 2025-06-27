48ce248c7e7a9172915570c75111cccfad8cb3b2
Marco Scazzari <marco.scazzari@lefonti.com>
2025-06-27 12:33:40 +0200

    Contact component v.1

---
+++ src/app/home/contact/contact.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Messaggio inviato:', this.contactForm.value);
+      this.contactForm.reset();
+    }
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,12 @@ import { FooterComponent } from '../core/footer/footer.component';
 import { ServicesComponent } from './services/services.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, HeaderComponent, FooterComponent, ServicesComponent, SkillsComponent, ProjectsComponent],
+  imports: [HeroComponent, HeaderComponent, FooterComponent, ServicesComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
