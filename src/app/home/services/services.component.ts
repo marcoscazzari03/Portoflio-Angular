c298148f67487240898a2aa6160513d68c6eaed0
Marco Scazzari <marco.scazzari@lefonti.com>
2025-11-10 14:18:22 +0100

    service component translated

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { SERVIZI } from './services.data';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslateModule],
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
- services = SERVIZI;
-
- }
+  services = SERVIZI;
+}
--- src/app/home/services/services.data.ts
+++ src/app/home/services/services.data.ts
@@ -1,23 +1,23 @@
-  export const SERVIZI = [
-    {
-      type: 'ux',
-      label: 'Ux/Ui',
-      title: 'User experience design',
-      description: `Uso le migliori tecnologie disponibili, unite a standard rigorosi per offrirti un sito web splendido, apprezzato dai tuoi utenti.`,
-      link: '/servizi/ux'
-    },
-    {
-      type: 'seo',
-      label: 'Seo',
-      title: 'Search engine optimization',
-      description: `Mi occupo di marketing digitale e SEO per aiutare le aziende a costruire una presenza online efficace, raggiungere risultati tangibili e crescere realmente.`,
-      link: '/servizi/seo'
-    },
-    {
-      type: 'web',
-      label: 'Web',
-      title: 'Web design & development',
-      description: `Mi occupo di web design e sviluppo, creando siti moderni, funzionali e ottimizzati per offrire la miglior esperienza utente su ogni dispositivo.`,
-      link: '/servizi/sviluppo'
-    }
-  ];
+export const SERVIZI = [
+  {
+    type: 'ux',
+    label: 'services.labels.ux',
+    title: 'services.titles.ux',
+    description: 'services.descriptions.ux',
+    link: '/servizi/ux'
+  },
+  {
+    type: 'seo',
+    label: 'services.labels.seo',
+    title: 'services.titles.seo',
+    description: 'services.descriptions.seo',
+    link: '/servizi/seo'
+  },
+  {
+    type: 'web',
+    label: 'services.labels.web',
+    title: 'services.titles.web',
+    description: 'services.descriptions.web',
+    link: '/servizi/sviluppo'
+  }
+];
