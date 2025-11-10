29f723dc84502fd9d003200062d9445ebb118067
Marco Scazzari <marco.scazzari@lefonti.com>
2025-11-10 14:30:49 +0100

    skill component translated

--- src/app/home/skills/skills.component.ts
+++ src/app/home/skills/skills.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SKILLS } from './skills.data';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
